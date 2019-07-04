f126396c572043dffd2e0ab6288574bcbcfb71d0
Emanuell Santos <dfmanu06@gmail.com>
2019-07-04 00:45:09 -0300

    DateHelper + Template Strings + ListModel

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,26 +6,34 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
 
         event.preventDefault();
-               
-        let data = new Date(
-            ...this._inputData.value.split('-').map((item, indice) => item - indice % 2)
-        );
-        console.log(data);
         
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaForm();
 
-        console.log(negociacao);
-        
+        console.log(this._listaNegociacoes.negociacoes);      
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);
+    }
+
+    _limpaForm() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 
 }
